1fedc0e40c9763882cdf93f85156c18eb2d6233c
Lenny Makori <makorilenny28@gmail.com>
2020-04-01 13:44:56 +0300

    create submit button

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -9,14 +9,7 @@ import { Quote } from '../quote';
 export class QuoteFormComponent implements OnInit {
 
 
-  newQuote = new Quote("", "", "");
-  @Output() addQuote = new EventEmitter<Quote>();
-
-  submitQuote(){
-    this.addQuote.emit(this.newQuote);
-    this.newQuote = new Quote("", "", "");
-
-  }
+  
 
   constructor() { }
 
